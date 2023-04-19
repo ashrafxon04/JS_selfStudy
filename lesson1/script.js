5f5afc38472b7211e28339ea196c32bb2e85d1ec
ashrafxon04 <ashrafyaxyoyev@gmail.com>
2023-04-19 14:12:45 +0500

    Variebles

---
+++ lesson1/script.js
@@ -0,0 +1,17 @@
+//Lesson 1
+
+//exercises1
+
+// let name = "Ilya"
+
+// console.log(`hello ${1}`); // hello 1
+// console.log(`hello ${name}`);// hello Ilya
+// console.log(`hello ${"name"}`);// hello name 
+
+//exercises2
+//==========================
+// let a = 1, b = 1;
+
+// let c =  ++a; // 2 "bir martaga oshirib beradi"
+// let d = b++; // 1  "o'z xolatida qoladi "
+// console.log(d , c);
