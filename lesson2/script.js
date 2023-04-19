cc9737711a359d1949d03521cef728e216779954
ashrafxon04 <ashrafyaxyoyev@gmail.com>
2023-04-19 14:18:58 +0500

    Type convirsion, Operators, comparision, Logical operators

---
+++ lesson2/script.js
@@ -0,0 +1,37 @@
+//lesson.2
+
+//exercises3
+//==========================
+// let a = 2;
+
+// let x = 1 + (a *= 2); //5  chunki "1 + (2 * 2)"
+
+// console.log(x);
+
+//exercises4
+//==========================
+// let q = "" + 1 + 0; // javob 10 chiqadi chunki string holatida kelyandi va 1 ga 0 ni qushyabdi.
+// let w = "" - 1 - 0; // javob -1 chiqadi number holatida chunki ushbu holatda stringn ignor qiladi.
+// let e = true + false // javob 1 chiqadi yani 1 + 0 = 1 holati ifodalanyabdi
+// let r = 6 / "3" // javob 2 chunki ushbu holatda 6 ni 3 ga bulyabd va number holatida javob qaytaryadi.
+// let t = "2" * "3" // javob 6 number holatida qaytadi 
+// let y = 4 + 5 + "px" // javob 9px qaytadi .
+// let u = "$" + 5 + 4 // javob  $54 qaytadi .
+// let i = "4px" - 2  // javob NaN qaytadi chunki ushubu holatda stringni numberdan ayrib bumdi.
+// let o = "-9" + 5 // javob -95 chiqadi string holatida .
+// let p = "-9" - 5 //  javob -14 chiqadi number holatida chunki ikta - (+) ga aylanib ketadi.
+// let a = null + 1 // javob 1 chiqadi chunki null da arifmetik amal bazjarsak buladi.
+// let s = undefined +1 // javob NaN chiqadi undefind da arifmetik amal bajarib bulmidi.
+// let d = "\t \n" - 2  // javob -2 chiqadi number holatida.
+// console.log(d);
+
+//exercises5
+//==========================
+// let  a = 5 > 4; // javob true qaytadi chunki haqqatda 5 , 4dan katta .
+// let b = "apple"  > "pineapple"; // javob false qaytadi chunki aski kodlar jadvali buyicha hisoblaydi.
+// let c = "2" > "12"; // javob true qaytadi chunki 1 chi kelgan son bn taqqoslaydi.
+// let d = undefined == null // javob true qaytadi chunki ikkalasida ham malumot yuq.
+// let e = undefined === null // javob false qaytadi.
+// let u = null == "\n0\n" // javob False qaytadi
+// let f = null === +"\n0\n" // javob False qaytadi
+// console.log(u , f);
