4a72d3b31d6a1037075de48f3b682db7ea64d314
ashrafxon04 <ashrafyaxyoyev@gmail.com>
2023-04-19 14:22:42 +0500

    If Else  , Nullish

---
+++ lesson3/script.js
@@ -0,0 +1,90 @@
+//lesson.3
+
+//exercises6
+//==========================
+// let number = "0"
+// if (number =="0"){
+//     console.log("hello"); // agar berilgan qiymat "0 ga teng bulmasa terminalda hchnma chiqmidi"
+// }
+
+//exercises7
+//==========================
+// let JavaScript = "EcmaScript"
+// if (JavaScript == "EcmaScript"){
+//     console.log("Right");
+// }else{ console.log("you don't know");
+
+// console.log(name);}
+
+// let JavaScript = "ECMAScript";
+// JavaScript == "ECMAScript" ? console.log("Right") : console.log("Your don't know");
+
+//exercises7
+//==========================
+
+// let a = 1;
+// let b = 2;
+
+// let result;
+// if (a + b < 4) {
+//     result = ("Bellow");
+// } else {
+//     result = ("Over");
+// }
+
+
+//turnory opetatot
+
+// a + b < 4 ? result = ("Bellow") : result = ("Over");
+
+// console.log(result);
+
+//exercises7
+//==========================
+
+// let number = -3
+
+// if (number < 0 ){
+//     console.log("berilgan son 0 dan kichik");
+// }else if (number === 0 ){
+//     console.log("berilgan son 0");
+// }else{
+//     console.log("berilgan son 0 dan katta");
+// }
+
+
+// let message ;
+// let login ="Director"
+// if (login == "Employee"){
+//     message = "hello";
+// }else if (login == "Director"){
+//     message = "greetings";
+// }else if (login == ""){
+//     message = "No Login"
+// }else{
+//     message = "";
+// }
+
+// Foydalanuvchidan yoshni so’raydigan dasturni tuzing. Yosh 50 dan y uqori bo’lsa, dastur “Siz kattasiz” deb chiqarib berishi kerak. Yosh 50 dan past bo’lsa “Siz yoshsiz” deb chiqarib berishi kerak.
+
+// function getRandomInt(max) {
+//     return Math.floor(Math.random() * 100);
+//   }
+//   if(getRandomInt() >= 50){
+//       console.log("Foydalanuvchining yoshi katta");
+//     }else{
+//         console.log("Foydalanuvchining yoshi kichik");
+// }
+// console.log(getRandomInt());
+
+
+
+//Somsani narxini so’raydigan dastur. Narx 3000 dan yuqori bo’lsa - dastur “qimmat” deb chiqarib bersin. 3000 dan arzon bo’lsa - “arzon” deb chiqarib bersin.
+
+// function getPriceSamsa(max) {
+//     let num = parseInt(Math.random() * max);
+//     console.log(num);
+//     num >= 3000 ? console.log('Somsaning narxi qimmat') : console.log('Somsaning narxi arzon');
+// }
+
+// getPriceSamsa(10000);
