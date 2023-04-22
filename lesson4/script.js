91b6b184a48a1aa25893bb2db80afdda55b74a81
ashrafxon04 <ashrafyaxyoyev@gmail.com>
2023-04-22 19:59:29 +0500

    For loop || switch case

---
+++ lesson4/script.js
@@ -0,0 +1,73 @@
+// lesson 4
+
+// switch || for loop 
+
+// function Printnumbers(){
+//     let i = 0;
+//     while (i < 10) {
+//        console.log("salom dunyo");
+//        i++;
+//     }
+//     }
+//     Printnumbers()                            // salom dunyo sozini 10 marta ekranga chqarish kodo yozildi
+
+
+//exercses 2
+//===========================
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log("karra jadvali");
+// for (let j = 1; j <= 10; j++) {
+//     console.log(`${i} * ${j} = ${i*j}`);    
+// }    
+// }                                         // karra jadval chiqarish kodi yozildi nested for loop orqali bunda birinchhi for 1 marta toliq aylanib chiqsa ikkinchi for 10 marta aylanib chiqadi.
+
+// for (let i = 0; i < 100; i++) {
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+// }                                           // juft sonlar chiqarish formulasi 
+// let browser = "Opera"
+// switch (browser) {
+//     case "Edge":
+//         console.log("you have got the Edge");
+//         break;
+//     case "chrome":
+//     case "Firefox":
+//     case "Safari":
+//     case "Opera":
+//         console.log("Okey we support these browsers too");
+//         break;
+//     default:
+//         console.log("we hope that page looks ok!");
+// }                                                     // ushbu holatda consol da chiqadigan javobni tushuntirib bering deyilgan.swetch case berilgan ifodani  baxolab yaniy tekshirib shungga togri keluvchi kod blokni ishga tushuradi togrimi.Ifdan farqi shundaki switch faqat tenglikni tekshiradi. yaniy qatiy tenglikni tekshiradi.
+
+// let a = 2;
+
+// if (a == 0){
+//     console.log(0);
+// }
+// if (a == 1) {
+//     console.log(1);
+// }
+// if (a == 2 || a == 3){
+//     console.log('2 , 3');
+// }                                // ushbu holatda berilgan shart qanotlantirilsa consol da natija chiqadi misol uchun birinchi shartda agat  a  ' 0 ' ga teng bulsa consolda "0" chiqsin deb shart berildi.
+
+
+//=======================================================================
+
+
+// for (let i = 1; i <= 100; i++) {
+//     if( i % 2 != 0){
+//         console.log(`${i} = 1 ga va uziga bulinadigan sonlar`);
+//     }if ( i % 2 == 0) {
+//         console.log(`${i} =  juft sonlar`);
+//     }
+// }                                   // ushbu formula 1 ga va uziga bulinadigan sonlat va juft sonlarni topish kod bloki yozilgan.!
+
+
+
+
+
+
