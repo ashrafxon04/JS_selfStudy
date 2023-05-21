d89fc59b7d56d450498a16e7522beb4af5af4657
ashrafxon04 <ashrafyaxyoyev@gmail.com>
2023-05-21 18:01:39 +0500

    String

---
+++ lesson7/script.js
@@ -0,0 +1,32 @@
+let str1 = "Webbrain "
+let str2 = "Academy "
+
+// console.log(str[str.length -1]);//javobi: 'n'  oxirgi harfni chiqarish !
+
+//string methods
+// console.log(str.toLowerCase()); //javob: "webbrain" hamma harflarni kichikga o'grib beradi
+// console.log(str.toLowerCase()); //javob: "webbrain" hamma harflarni kichikga o'grib beradi
+// console.log(str1.startsWith("W")); //javob: "true" text bosh harfi haqqatda "W" bn boshlanyabdi 
+// console.log(str.endsWith("n")); //javob: "true" text oxirgi harfi haqqatda "n" bn tugayabdi 
+// console.log(str1.padStart(10 , "d")); //javob: "ddWebbrain" boshidan malumot qoshish uchun ( 1 chi qiymat uzunlig "length" , 2 chi qiymat nma qo'shishligi)
+// console.log(str.padEnd(10 , "d")); //javob: "Webbraindd" oxirdan malumot qoshish uchun ( 1 chi qiymat uzunlig "length" , 2 chi qiymat nma qo'shishligi)
+// console.log(str1.replace("Web" , "Space")); //javob: "Spacebrain" "web" sozini "space" ga uzgartirib beryabdi.
+// console.log(str1.repeat(10)); // javob: bu sozni berilan qiymatga kora kopaytirib beradi!
+// console.log(str1.concat(str2)); // javob: webbrain academy concat yaniy bir uzgaruvchini ikkinchi uzgaruvchiga qushib beradi!
+// console.log(str1.includes("brain")); // javob: "true" bu method keltirilgan malumot ichida berilgan harf yoki soz bulsa boolean qiymat qaytaradi "true" or "false"
+// console.log(str1.indexOf("b")); // javob: "2" chunki b harfi index buyicha 2 chi da turibdi bu method index buyicha chqaradi
+// console.log(str1.lastIndexOf("b")); // javob: "3" chunki b harfi  oxirgi index buyicha 3 chi da turibdi bu method  oxirgi index buyicha chqaradi!
+// console.log(str1.charAt(5)); // javob "a"  berilgan index da qaysi harf borligini aniqlaydi
+// console.log(str1.substring( 2 , 6)); // javob "bbrain" 2 ta qiymat oladi 1 chisi nechinchi index da boshlanshi 2 chi nechinchi index gacha olishi
+// console.log(str1.slice( 2 , 5)); // javob "bbra"  2 ta qiymat oladi 1 chisi nechinchi index da boshlanshi 2 chi nechinchi index gacha olishi
+// console.log(str1.substr(2 ,5)); // javob "bbrai" 1 chi index 2 chi lengthagar bitta index uzi berilgan bulsa oxirgacha oladi
+
+
+// let obj = {
+//     name: "webbrain",
+//     title: "IT",
+//     age: 18 
+// }
+// let {name,title,age} = obj
+// console.log(name);          // distructude qilish usuli
+
